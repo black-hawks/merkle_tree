3f1ab19b50c4c5bdfdb865950a7315a30a2edc95
VivekMurarkaIndIre <vivek.murarka@ucdconnect.ie>
2023-03-18 21:54:34 +0000

    Added JavaDoc for NodeDirection adn MerkleTeeNode

--- src/main/java/blockchainMerkleTree/dataStructure/merkleTree/NodeDirection.java
+++ src/main/java/blockchainMerkleTree/dataStructure/merkleTree/NodeDirection.java
@@ -1,6 +1,14 @@
 package blockchainMerkleTree.dataStructure.merkleTree;
 
-public enum NodeDirection {
+/**
+ An enumeration representing the possible directions of a node in a binary tree structure.
+ Each node in a binary tree can have a left and/or right child node, and this enumeration defines
+ the possible directions in which a node can have a child node.
+ **/
+ public enum NodeDirection {
+
+     //The left child direction of a node in a binary tree.
     LEFT,
+    // The right child direction of a node in a binary tree.
     RIGHT
-}
+ }
